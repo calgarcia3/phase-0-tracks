3f701be8eb6a162ae4c15f426f5641251554692e
Calvin Garcia <cgarcia3ftl@gmail.com>
2016-08-28 15:45:18 -0600

    vampires.rb added conditional statements on garlic, sunlight and a questionaire to expose possible vampire canidates.

---
+++ ruby/vampires.rb
@@ -0,0 +1,16 @@
+puts "What is your name?"
+name = gets.chomp
+puts "How old are you?"
+age = gets.chomp
+puts "What year were you born?"
+year_born = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+answer = gets.chomp
+puts "Would you like to enroll in the company's health insurance?"
+health = gets.chomp
+
+wolves_like_sunshine = true
+wolves_like_garlic = true
+vampires_like_sunshine = false
+vampires_like_garlic = false
+
