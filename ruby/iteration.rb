babf73910488786caa8108d3527512664b279b5f
Calvin Garcia <cgarcia3ftl@gmail.com>
2016-09-08 23:05:32 -0600

    Did not add before

---
+++ ruby/iteration.rb
@@ -0,0 +1,21 @@
+def car
+	puts "I like cars"
+	yield("dodge", "Civic")
+end
+
+car { |car1, car2| puts "I have two cars, they are #{car1} and #{car2}" }
+
+locations = ["texas", "florida", "california"]
+
+locations.each do |states|
+	puts "I have visited #{states}"
+	locations.next
+end
+
+
+
+car_types = { 'ford' => 'F-250', 'dodge' => 'avenger', 'chevy' => 'corvette'}
+
+car_types.each do |make, model|
+	puts "I own a #{make} #{model}."
+end
