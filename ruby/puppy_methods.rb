7c52e67fc02d06898b4c512c022f269500b7769e
Calvin Garcia <cgarcia3ftl@gmail.com>
2016-09-11 00:30:35 -0600

    added methods (fetch, speak, roll_over, dog_years, shake) added call outs for methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -4,6 +4,22 @@ class Puppy
     puts "I brought back the #{toy}!"
     toy
   end
+  def speak
+  	puts "Woof!".times(7)
+  end
+  def roll_over
+  	puts "rolls over"
+  end
+  def dog_years(human_years)
+  	puts human_years * 7
+  end
+  def shake
+  	puts "rolls over"
+  end
 
 end
 
+dogs = Puppy.new 
+dogs.fetch("cat")
+dogs.dog_years(7)
+dogs.shake
