2ec8c68bafaca58f632745d544ccc5238a622b62
Calvin Garcia <cgarcia3ftl@gmail.com>
2016-09-11 00:50:46 -0600

    Added methods for initialize

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,21 +5,25 @@ class Puppy
     toy
   end
   def speak
-  	puts "Woof!".times(7)
+    puts "Woof!" * 7
   end
   def roll_over
-  	puts "rolls over"
+    puts "rolls over"
   end
   def dog_years(human_years)
-  	puts human_years * 7
+    puts human_years * 7
   end
   def shake
-  	puts "rolls over"
+    puts "rolls over"
   end
-
+  def initialie
+    puts "Initializing new puppy instance"
+  end
 end
 
 dogs = Puppy.new 
 dogs.fetch("cat")
 dogs.dog_years(7)
+dogs.speak
 dogs.shake
+dogs.roll_over
