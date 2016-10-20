87228f748c882795901d478abd9f8123955736c4
Calvin Garcia <cgarcia3ftl@gmail.com>
2016-10-20 17:01:12 -0600

    added Athlete class with sprint high_jump & initialize def's

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -16,7 +16,7 @@ class Puppy
   def shake
     puts "rolls over"
   end
-  def initialie
+  def initialis
     puts "Initializing new puppy instance"
   end
 end
@@ -27,3 +27,29 @@ dogs.dog_years(7)
 dogs.speak
 dogs.shake
 dogs.roll_over
+
+class Athlete
+
+  def sprint
+    puts "Ran 100 meters"
+  end
+
+  def high_jump
+    puts "Jumped 8'4"
+  end
+
+  def initialize
+    puts "initializing Athlete instance"
+  end
+
+end
+
+track = Athlete.new
+
+do track.each
+  if track.each <= 50
+    puts track
+  else
+    puts "all Done"
+  end
+end
