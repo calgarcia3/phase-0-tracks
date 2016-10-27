4128e97755d15fc6cd6bc8fe893edf2860214d01
Calvin Garcia <cgarcia3ftl@gmail.com>
2016-10-26 23:23:49 -0600

    Went through week 6 as practice,

---
+++ ruby/list/todo_list.rb
@@ -0,0 +1,18 @@
+class Todo_list
+	def initialize(list)
+		#stores list items given on initialization
+		list = []
+	end
+	def add_item(item)
+		# adds an item to the list
+
+	end
+	def delete_item
+		# deletes an item
+	end
+
+	def retrieve
+		#retrieves an item by index
+	end
+end
+
