cfc1eb12bae1b441134a29803e2fbb4c3932e96a
Calvin Garcia <cgarcia3ftl@gmail.com>
2016-10-31 09:44:23 -0600

    Added psuedocode and .reverse call and function.

---
+++ js/explore.js
@@ -0,0 +1,10 @@
+// create a function that takes a string as a parameter
+//then reverses it..
+// create a function that takes a string as a parameter
+// Reverse each
+
+
+class HelloWorldPrinter
+              implements Printable { ... }
+...
+job.setPrintable(new HelloWorldPrinter());
